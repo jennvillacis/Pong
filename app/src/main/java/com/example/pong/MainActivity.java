5028ebedb5a3ed1da428882c9f6fd3b07122bcbd
Asia <Joanna.gromadzka@seattlecentral.edu>
2019-08-12 21:11:40 -0700

    add 2nd Player
    adjusting appearance

--- app/src/main/java/com/example/pong/MainActivity.java
+++ app/src/main/java/com/example/pong/MainActivity.java
@@ -1,15 +1,16 @@
 package com.example.pong;
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //  openDialog();
+
     }
 
     public void Player1(View view) {
@@ -17,30 +18,9 @@ public class MainActivity extends AppCompatActivity {
         startActivity(new Intent(this,PlayerName.class));
     }
 
-
-
-    /* *** This is a pop up window at the end of the level*****
-
-    public void openDialog(View view) {
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-        alertDialogBuilder.setMessage("Congrats!You can go to next level!");
-
-        alertDialogBuilder.setPositiveButton("yes", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface arg0, int arg1) {
-                Toast.makeText(MainActivity.this, "You clicked yes button", Toast.LENGTH_LONG).show();
-            }
-        });
-
-        alertDialogBuilder.setNegativeButton("No, exit", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                finish();
-            }
-        });
-
-        AlertDialog alertDialog = alertDialogBuilder.create();
-        alertDialog.show();
-    }*/
+    public void Player2(View view) {
+        setContentView(R.layout.activity_player_name);
+        startActivity(new Intent(this,PlayerName.class));
+    }
 
 }
